da6b6dada18cee757b8ba4568f46d7341ce6f08b
girlsgotomarz <alicia.n.blair@gmail.com>
2023-03-08 10:07:26 -0600

    added api temperatures/ conditions

--- src/app.js
+++ src/app.js
@@ -4,10 +4,17 @@ function displayTemperature(response) {
     let cityElement = document.querySelector("#city");
     let temperatureElement = document.querySelector("#temperature");
     let descriptionElement = document.querySelector("#description");
+    let feelsLikeElement = document.querySelector("#feels-like");
+    let humidityElement = document.querySelector("#humidity");
+    let windElement = document.querySelector("#wind");
+
 
     cityElement.innerHTML = response.data.city;
     temperatureElement.innerHTML = Math.round(response.data.temperature.current);
     descriptionElement.innerHTML = response.data.condition.description;
+    feelsLikeElement.innerHTML = Math.round(response.data.temperature.feels_like);
+    humidityElement.innerHTML = Math.round(response.data.temperature.humidity);
+    windElement.innerHTML = Math.round(response.data.wind.speed);
 }
 
 let apiKey = "tce447e9aac51b35b06befo3864f3df5";
