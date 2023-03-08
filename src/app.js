009df381612ec38590dde7e5b16c20b630a86d34
girlsgotomarz <alicia.n.blair@gmail.com>
2023-03-08 10:47:41 -0600

    created api time

--- src/app.js
+++ src/app.js
@@ -1,7 +1,46 @@
+function formatDate(timestamp) {
+    let date = new Date(timestamp);
+    let hours = date.getHours();
+    
+    if (hours < 10) {
+        hours = `0${hours}`;
+    }
+    let minutes = date.getMinutes();
+    if (minutes < 10) {
+        minutes = `0${minutes}`;
+    }
+
+    let days = [
+        "Sunday", 
+        "Monday", 
+        "Tuesday", 
+        "Wednesday", 
+        "Thursday", 
+        "Friday", 
+        "Saturday", 
+        "Sunday"];
+
+    let day = days[date.getDay()];
+    return `${day} ${hours}:${minutes}`;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
 function displayTemperature(response) {
 
     console.log(response.data);
     let cityElement = document.querySelector("#city");
+    let dateElement = document.querySelector("#date");
     let temperatureElement = document.querySelector("#temperature");
     let descriptionElement = document.querySelector("#description");
     let feelsLikeElement = document.querySelector("#feels-like");
@@ -10,6 +49,7 @@ function displayTemperature(response) {
 
 
     cityElement.innerHTML = response.data.city;
+    dateElement.innerHTML = formatDate(response.data.time * 1000);
     temperatureElement.innerHTML = Math.round(response.data.temperature.current);
     descriptionElement.innerHTML = response.data.condition.description;
     feelsLikeElement.innerHTML = Math.round(response.data.temperature.feels_like);
