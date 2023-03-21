b678666e8da2cd7547263315e640593cf8f9bbe4
girlsgotomarz <alicia.n.blair@gmail.com>
2023-03-21 10:21:40 -0500

    replaced fake data with JS real data

--- src/app.js
+++ src/app.js
@@ -24,31 +24,44 @@ function formatDate(timestamp) {
     return `${day} ${hours}:${minutes}`;
 }
 
+function formatDay(timestamp) {
+  let date = new Date(timestamp * 1000);
+  let day = date.getDay();
+  let days = ["Sun", "Mon", "Tues", "Wed", "Thurs", "Fri", "Sat"];
+
+  return days[day];
+
+}
+
 function displayForecast(response) {
-  console.log(response.data.daily);
+  
+    let forecast = response.data.daily;
+
     let forecastElement = document.querySelector("#forecast");
   
-    let days = ["Tues", "Wed", "Thurs", "Fri", "Sat"];
+    
   
     let forecastHTML = `<div class="row">`;
-    days.forEach(function (day) {
+    forecast.forEach(function (forecastDay, index) {
+      if (index < 6) {
       forecastHTML =
         forecastHTML +
         `
         <div class="col-2">
-          <div class="weather-forecast-date">${day}</div>
+          <div class="weather-forecast-date">${formatDay(forecastDay.time)}</div>
           <img
-            src="http://openweathermap.org/img/wn/50d@2x.png"
-            alt=""
+            src= ${forecastDay.condition.icon_url}
+            alt="icon"
             width="42"
           />
           <div class="weather-forecast-temperatures">
-            <span class="weather-forecast-temperature-max"> 18° </span>
-            <span class="weather-forecast-temperature-min"> 12° </span>
+            <span class="weather-forecast-temperature-max"> ${Math.round(forecastDay.temperature.maximum)}° </span>
+            <span class="weather-forecast-temperature-min"> ${Math.round(forecastDay.temperature.minimum)}° </span>
           </div>
         </div>
-    `;
+    `;}
     });
+  
   
     forecastHTML = forecastHTML + `</div>`;
     forecastElement.innerHTML = forecastHTML;
